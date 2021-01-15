e3d0333cb80a3a2eb307b1da46c9673b2ecf88f0
Or Toledano <ortoledano@protonmail.com>
2021-01-15 10:39:49 +0200

    configs

--- run_double.sh
+++ run_double.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1,$2 TORCH_MODEL_ZOO=../torch ../myenv37/bin/python run.py --config config/taichi-256-downscale.yaml --device_ids 0,1
+CUDA_VISIBLE_DEVICES=$1,$2 TORCH_MODEL_ZOO=../torch ../myenv37/bin/python run.py --config config/taichi-256.yaml --device_ids 0,1
--- run_rec_ours.sh
+++ run_rec_ours.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1 TORCH_MODEL_ZOO=../torch ../myenv37/bin/python run.py --config config/taichi-256-downscale.yaml --mode reconstruction --checkpoint "/home/dcor/ronmokady/workshop21/team3/animation-with-keypoint-mask/log/taichi-256-downscale 10_01_21_15.51.28/00000099-checkpoint.pth.tar"
+CUDA_VISIBLE_DEVICES=$1 TORCH_MODEL_ZOO=../torch ../myenv37/bin/python run.py --config config/taichi-256.yaml --mode reconstruction --checkpoint "/home/dcor/ronmokady/workshop21/team3/animation-with-keypoint-mask/log/taichi-256-downscale 10_01_21_15.51.28/00000099-checkpoint.pth.tar"
