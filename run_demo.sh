5615fdb8c544c98f8d4eb96c63ceb4ced3163856
Or Toledano <ortoledano@localhost.localdomain>
2021-01-15 10:35:23 +0200

    bash scripts

---
+++ run_demo.sh
@@ -0,0 +1,2 @@
+python demo.py  --config config/taichi-256.yaml --driving_video "in.mp4" --source_image "/home/dcor/datasets/taichi//test/A3ZmT97hAWU#008544#008731.mp4/0000186.png"  --checkpoint /home/dcor/ronmokady/workshop21/team3/cpk/taichi-cpk.pth.tar --relative --adapt_scale
+
---
+++ run_rec.sh
@@ -0,0 +1,1 @@
+../myenv37/bin/python run.py --config config/taichi-256.yaml --mode reconstruction --checkpoint /home/dcor/ronmokady/workshop21/team3/cpk/taichi-cpk.pth.tar
