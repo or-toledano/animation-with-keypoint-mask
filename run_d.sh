f40270b98d2c8925f8b76e235833759b4d8d21f2
Or Toledano <ortoledano@protonmail.com>
2021-02-09 16:28:17 +0200

    update run scripts

--- run_d.sh
+++ run_d.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1,$2 TORCH_MODEL_ZOO=../torch ../myenv37/bin/python run.py --config config/taichi-256-d.yaml --device_ids 0,1
+CUDA_VISIBLE_DEVICES=$1,$2 TORCH_MODEL_ZOO=../torch ../myenv37/bin/python run.py --config config/taichi-256-d.yaml --checkpoint_with_kp ../cpk/taichi-cpk.pth.tar --device_ids 0,1
--- run_q.sh
+++ run_q.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1,$2,$3,$4 TORCH_MODEL_ZOO=../torch ../python run.py --config config/taichi-256-q.yaml --device_ids 0,1,2,3
+CUDA_VISIBLE_DEVICES=$1,$2,$3,$4 TORCH_MODEL_ZOO=../torch ../python run.py --config config/taichi-256-q.yaml  --checkpoint_with_kp ../cpk/taichi-cpk.pth.tar --device_ids 0,1,2,3
