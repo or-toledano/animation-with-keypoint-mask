c7c3100e61045c021f6c8c8d2199583f5db45c84
Or Toledano <ortoledano@protonmail.com>
2021-02-12 15:16:55 +0200

    Removal of kp normalization

--- animate.py
+++ animate.py
@@ -5,32 +5,8 @@ from torch.utils.data import DataLoader
 from frames_dataset import PairedDataset
 from logger import Logger, Visualizer
 import imageio
-from scipy.spatial import ConvexHull
 import numpy as np
 from sync_batchnorm import DataParallelWithCallback
-
-
-def normalize_kp(kp_source, kp_driving, kp_driving_initial, adapt_movement_scale=False,
-                 use_relative_movement=False, use_relative_jacobian=False):
-    if adapt_movement_scale:
-        source_area = ConvexHull(kp_source['value'][0].data.cpu().numpy()).volume
-        driving_area = ConvexHull(kp_driving_initial['value'][0].data.cpu().numpy()).volume
-        adapt_movement_scale = np.sqrt(source_area) / np.sqrt(driving_area)
-    else:
-        adapt_movement_scale = 1
-
-    kp_new = {k: v for k, v in kp_driving.items()}
-
-    if use_relative_movement:
-        kp_value_diff = (kp_driving['value'] - kp_driving_initial['value'])
-        kp_value_diff *= adapt_movement_scale
-        kp_new['value'] = kp_value_diff + kp_source['value']
-
-        if use_relative_jacobian:
-            jacobian_diff = torch.matmul(kp_driving['jacobian'], torch.inverse(kp_driving_initial['jacobian']))
-            kp_new['jacobian'] = torch.matmul(jacobian_diff, kp_source['jacobian'])
-
-    return kp_new
 
 
 def animate(config, generator, kp_detector, checkpoint, log_dir, dataset):
@@ -68,18 +44,14 @@ def animate(config, generator, kp_detector, checkpoint, log_dir, dataset):
             source_frame = x['source_video'][:, :, 0, :, :]
 
             kp_source = kp_detector(source_frame)
-            kp_driving_initial = kp_detector(driving_video[:, :, 0])
 
             for frame_idx in range(driving_video.shape[2]):
                 driving_frame = driving_video[:, :, frame_idx]
                 kp_driving = kp_detector(driving_frame)
-                kp_norm = normalize_kp(kp_source=kp_source, kp_driving=kp_driving,
-                                       kp_driving_initial=kp_driving_initial, **animate_params['normalization_params'])
-                out = generator(source_frame, kp_source=kp_source, kp_driving=kp_norm)
+                out = generator(source_frame, kp_source=kp_source, kp_driving=kp_driving)
 
                 out['kp_driving'] = kp_driving
                 out['kp_source'] = kp_source
-                out['kp_norm'] = kp_norm
 
                 predictions.append(np.transpose(out['low_res_prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
                 predictions.append(np.transpose(out['upscaled_prediction'].data.cpu().numpy(), [0, 2, 3, 1])[0])
