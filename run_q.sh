99dcc93a346c6f6c9d33fdbdb885068311981a41
Or Toledano <ortoledano@protonmail.com>
2021-02-22 18:01:34 +0200

    scripts

--- run_q.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=$1,$2,$3,$4 TORCH_MODEL_ZOO=../torch ../python run.py --config config/taichi-256-q.yaml --device_ids 0,1,2,3
--- run_q_kp30.sh
+++ run_q_kp30.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=$1,$2,$3 TORCH_MODEL_ZOO=../torch ../python37 run.py --config config/taichi-256-q-kp30.yaml --device_ids 0,1,2
+CUDA_VISIBLE_DEVICES=$1,$2,$3,$4 TORCH_MODEL_ZOO=../torch ../python37 run.py --config config/taichi-256-q-kp30.yaml --device_ids 0,1,2,3
